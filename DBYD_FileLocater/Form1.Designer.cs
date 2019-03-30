c87dfb4a7eaa84041c73ca3ef4a6c0cd7232c2ed
Adam Unger <adamunger0@gmail.com>
2019-03-29 21:21:30 -0600

    Initial commit.

---
+++ DBYD_FileLocater/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace DBYD_FileLocater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dbydListBox = new System.Windows.Forms.ListBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.moveButton = new System.Windows.Forms.Button();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.relatedFilesListBox = new System.Windows.Forms.ListBox();
+            this.dbydFilenameInput = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(1176, 35);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(123, 30);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(61, 29);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(146, 30);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33332F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dbydListBox, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.relatedFilesListBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.dbydFilenameInput, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 35);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 54F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 54F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 54F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1176, 674);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(579, 54);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dial Before You Dig Projects";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(591, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(581, 54);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Related Files";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // dbydListBox
+            // 
+            this.dbydListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dbydListBox.FormattingEnabled = true;
+            this.dbydListBox.HorizontalScrollbar = true;
+            this.dbydListBox.ItemHeight = 20;
+            this.dbydListBox.Location = new System.Drawing.Point(4, 59);
+            this.dbydListBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dbydListBox.Name = "dbydListBox";
+            this.dbydListBox.Size = new System.Drawing.Size(579, 502);
+            this.dbydListBox.TabIndex = 5;
+            this.dbydListBox.SelectedIndexChanged += new System.EventHandler(this.dbydListBox_SelectedIndexChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.flowLayoutPanel1, 2);
+            this.flowLayoutPanel1.Controls.Add(this.moveButton);
+            this.flowLayoutPanel1.Controls.Add(this.refreshButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(4, 625);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1168, 44);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // moveButton
+            // 
+            this.moveButton.Location = new System.Drawing.Point(1052, 5);
+            this.moveButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.moveButton.Name = "moveButton";
+            this.moveButton.Size = new System.Drawing.Size(112, 35);
+            this.moveButton.TabIndex = 1;
+            this.moveButton.Text = "Move";
+            this.moveButton.UseVisualStyleBackColor = true;
+            this.moveButton.Click += new System.EventHandler(this.moveButton_Click);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(932, 5);
+            this.refreshButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(112, 35);
+            this.refreshButton.TabIndex = 2;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // relatedFilesListBox
+            // 
+            this.relatedFilesListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.relatedFilesListBox.FormattingEnabled = true;
+            this.relatedFilesListBox.HorizontalScrollbar = true;
+            this.relatedFilesListBox.ItemHeight = 20;
+            this.relatedFilesListBox.Location = new System.Drawing.Point(591, 59);
+            this.relatedFilesListBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.relatedFilesListBox.Name = "relatedFilesListBox";
+            this.relatedFilesListBox.Size = new System.Drawing.Size(581, 502);
+            this.relatedFilesListBox.TabIndex = 7;
+            // 
+            // dbydFilenameInput
+            // 
+            this.dbydFilenameInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dbydFilenameInput.Location = new System.Drawing.Point(4, 571);
+            this.dbydFilenameInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dbydFilenameInput.Name = "dbydFilenameInput";
+            this.dbydFilenameInput.Size = new System.Drawing.Size(579, 26);
+            this.dbydFilenameInput.TabIndex = 8;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.removeButton);
+            this.flowLayoutPanel2.Controls.Add(this.addButton);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(591, 571);
+            this.flowLayoutPanel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(581, 44);
+            this.flowLayoutPanel2.TabIndex = 9;
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(4, 5);
+            this.removeButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(112, 35);
+            this.removeButton.TabIndex = 0;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(124, 5);
+            this.addButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(112, 35);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1176, 709);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "DBYD File Finder";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox dbydListBox;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button moveButton;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.ListBox relatedFilesListBox;
+        private System.Windows.Forms.TextBox dbydFilenameInput;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button addButton;
+    }
+}
+
---
+++ DBYD_FileLocater/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBYD_FileLocater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //refreshButton_Click(null, null);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private List<string> getFilenames(string match)
+        {
+            // Ensure that the input match isn't empty or null.
+            if (string.IsNullOrEmpty(match) || string.IsNullOrWhiteSpace(match))
+            {
+                return new List<string>();
+            }
+            
+            // Set a variable to the current users root profile directory and begin search from there.
+            string root = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+            // To hold the final list of strings.
+            var files = new List<string>();
+
+            // Get the fully qualified path name
+            var rootDirectory = new DirectoryInfo(root);
+            root = rootDirectory.FullName;
+
+            // Variables for the while loop to ensure that we skip folders for which this program doesn't have permission.
+            var folders = new Queue<string>();
+            folders.Enqueue(root);
+
+            // Iterate through the list of folders beginning with root and find all of the files that match the specified file name criteria.
+            while (folders.Count != 0)
+            {
+                string currentFolder = folders.Dequeue();
+
+                // Get all of the matching files from the current directory
+                try
+                {
+                    // TODO: change this string to match the project file names.....
+                    var currentFiles = Directory.EnumerateFiles(currentFolder, match);
+                    files.AddRange(currentFiles);
+                }
+                // Ignore these exceptions
+                catch (UnauthorizedAccessException uae)
+                {
+                    //Console.WriteLine(uae.Message);
+                }
+                catch (PathTooLongException ptle)
+                {
+                    //Console.WriteLine(ptle.Message);
+                }
+                catch (IOException ioe)
+                {
+                    //DO Something...
+                }
+
+                // Attempt to open all of the subdirectories from the current one. Ignore errors.
+                try
+                {
+                    var currentSubFolders = Directory.GetDirectories(currentFolder);
+                    foreach (string current in currentSubFolders)
+                    {
+                        folders.Enqueue(current);
+                    }
+                }
+                // Ignore these exceptions
+                catch (UnauthorizedAccessException uae)
+                {
+                    //Console.WriteLine(uae.Message);
+                }
+                catch (PathTooLongException ptle)
+                {
+                    //Console.WriteLine(ptle.Message);
+                }
+                catch (IOException ioe)
+                {
+                    // Do Shit....
+                }
+            }
+
+            return files;
+        }
+
+        private void dbydListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Get the currently selected item in the dbyd ListBox.
+            string curItem = dbydListBox.SelectedItem.ToString();
+
+            // Clear the related files listbox
+            relatedFilesListBox.Items.Clear();                
+
+            // Create the string that we want to match files to from the master string
+            var match = Path.GetFileNameWithoutExtension(curItem);
+
+            var files = getFilenames(match + ".txt");
+
+            if (files.Count() < 1)
+            {
+                relatedFilesListBox.Items.Add("<None Found>");
+            }
+            else
+            {
+                // Display the previously created list of files in the listview.
+                foreach (var f in files)
+                {
+                    relatedFilesListBox.Items.Add(f);
+                }
+            }
+
+            if (!string.IsNullOrEmpty(match) && !string.IsNullOrWhiteSpace(match))
+            {
+                // Find all related file names
+
+                // Display those files
+            }
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            // Make the mouse a spinning wheel to indicate processing in progress.
+            //Application.UseWaitCursor = false;
+
+            // Start by clearing the listview to avoid duplicate entry's.
+            dbydListBox.Items.Clear();
+
+            var inputMatch = dbydFilenameInput.Text;
+            if (string.IsNullOrEmpty(inputMatch) || string.IsNullOrWhiteSpace(inputMatch))
+            {
+                dbydListBox.Items.Add("<None Found>");
+                return;
+            }
+
+            //var files = getFilenames("*test*.pdf");
+            var files = getFilenames(inputMatch);
+
+            if (files.Count() < 1)
+            {
+                dbydListBox.Items.Add("<None Found>");
+            }
+            else
+            {
+                // Display the previously created list of files in the listview.
+                foreach (var f in files)
+                {
+                    dbydListBox.Items.Add(f);
+                }
+            }
+        }
+
+        private void moveButton_Click(object sender, EventArgs e)
+        {
+            // Get the currently selected item in the dbyd ListBox.
+            string curItem = dbydListBox.SelectedItem.ToString();
+
+            // Create the directory for the files it it doesn't already exist.
+            var newDir = Path.ChangeExtension(curItem, null);
+            newDir = newDir + "_responses";
+            try
+            {
+                if (!Directory.Exists(newDir))
+                {
+                    DirectoryInfo di = Directory.CreateDirectory(newDir);
+                }
+            }
+            catch (Exception)
+            {
+                // TODO: print an error message...
+            }            
+
+            // Move all of the files from the related files listbox into the new directory.
+            foreach (var f in relatedFilesListBox.Items)
+            {
+                try
+                {
+                    File.Move(f.ToString(), newDir + "\\" + Path.GetFileName(f.ToString()));
+                }
+                catch (Exception)
+                {
+                    // TODO: print an error message...
+                }
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Configure message box
+            string message = "Created by Adam Unger\nDBYD File Locater v1.0\nEmail: adamunger0@gmail.com";
+            // Show message box
+            var result = MessageBox.Show(message);
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            relatedFilesListBox.Items.Remove(relatedFilesListBox.SelectedItem);
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog selectFileDialog = new OpenFileDialog();
+            if (selectFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var fNames = selectFileDialog.FileNames;
+                foreach (var fName in fNames)
+                {
+                    if (!string.IsNullOrEmpty(fName) && !string.IsNullOrWhiteSpace(fName))
+                    {
+                        relatedFilesListBox.Items.Add(fName);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
